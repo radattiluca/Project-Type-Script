ee923bb5307e7457dcb534b6a98f35f53246cea4
radattiluca <radattiluca@gmail.com>
2025-02-01 15:23:23 +0100

    added dist at gitignore

--- dist/clienti.js
+++
@@ -1,7 +0,0 @@
-//clienti.js
-import { Cliente } from "./class.js";
-export { cliente001, cliente002, cliente003, cliente004 };
-let cliente001 = new Cliente("Luca", "Bianchi", "lucabianchi@email.com", "PayPal");
-let cliente002 = new Cliente("Francesca", "Schianchi", "francescaschianchi@email.com", "Bonifico");
-let cliente003 = new Cliente("Alessandro", "Vitale", "alessandrovitale@email.com", "Carta di Credito");
-let cliente004 = new Cliente("Elisa", "Tosse", "tosseelisa@email.com", "PayPal");
